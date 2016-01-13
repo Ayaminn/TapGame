27fc2037ad5dd1fd6dc66cde725e4b52727220bd
Ayaminn <ayamin.imai@gmail.com>
2016-01-13 20:52:51 +0900

    キャラクタースクリプト　エラー発生中

--- Assets/Scripts/CharacterMain.cs
+++ Assets/Scripts/CharacterMain.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MainChar : MonoBehaviour {
+public class CharacterMain : MonoBehaviour {
+
+	public Rigidbody rb;
+	public float jump;
 
 	// Use this for initialization
 	void Start () {
-	
+		rb = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetKey(KeyCode.Space)){
+			rb.AddForce(transform.forward * jump);
+		}
 	}
 }
