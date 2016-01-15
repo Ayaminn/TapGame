a6befcba44409a833ba13afe209d2452ee2994f3
Ayaminn <ayamin.imai@gmail.com>
2016-01-15 21:37:54 +0900

    バグの修正

--- Assets/Scripts/CharacterMain.cs
+++ Assets/Scripts/CharacterMain.cs
@@ -13,7 +13,7 @@ public class CharacterMain : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey(KeyCode.Space)){
+		if(Input.GetKeyDown(KeyCode.Space)){
 			rb.AddForce(transform.up * jump);
 		}
 	}
