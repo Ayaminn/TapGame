ac81f02741bec0ef95754480639544a7d832387b
Ayaminn <ayamin.imai@gmail.com>
2016-01-27 20:01:12 +0900

    2Dと3D切り替え・ステージ拡張

--- Assets/Scripts/CharacterMain.cs
+++ Assets/Scripts/CharacterMain.cs
@@ -5,17 +5,63 @@ public class CharacterMain : MonoBehaviour {
 
 	public Rigidbody rb;
 	public float jump;
+	Camera _2DCamera;
+	Camera _3DCamera;
+	public bool camera2D = true; 
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+		_2DCamera = GameObject.Find("2D Camera").GetComponent<Camera>();
+		_3DCamera = GameObject.Find ("3D Camera").GetComponent<Camera> ();
+
+		_3DCamera.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//スペースキーで上ジャンプ
-		if(Input.GetKeyDown(KeyCode.Space)){
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			//ジャンプ
 			rb.AddForce(transform.up * jump);
+		}
+
+		if (Input.GetKeyDown(KeyCode.A)) {
+			//カメラ切り替え
+			if(_2DCamera.enabled){
+				_2DCamera.enabled = false;
+				_3DCamera.enabled = true;
+
+				camera2D = false;
+			}else{
+				_2DCamera.enabled = true;
+				_3DCamera.enabled = false;
+
+				camera2D = true;
+			}
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			if (camera2D == true){
+				transform.position += new Vector3 (0.1f, 0, 0);
+			} else {
+				transform.position += new Vector3 (0, 0, -0.1f);
+			}
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			if (camera2D == true){
+				transform.position += new Vector3 (-0.1f, 0, 0);
+			} else {
+				transform.position += new Vector3 (0, 0, 0.1f);
+			}
+		}
+
+		if (Input.GetKey(KeyCode.UpArrow) && camera2D == false){
+			transform.position += new Vector3 (0.1f, 0, 0);
+		}
+
+		if (Input.GetKey(KeyCode.DownArrow) && camera2D == false){
+			transform.position += new Vector3 (-0.1f, 0, 0);
 		}
 	}
 }
